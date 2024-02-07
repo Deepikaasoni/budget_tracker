2a18d5f3506da425a089c5ff504b65f012b84358
Deepikaasoni <109984417+Deepikaasoni@users.noreply.github.com>
2024-02-07 12:32:46 +0530

    This is my fifth commit

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -7,12 +7,12 @@ export const TransactionList = () => {
   
 
   return (
-    <div>
+    <>
       <h3>History</h3>
       <ul  className="list">
         {transactions.map(transaction => (<Transaction key ={transaction.id} transaction= {transaction}  />))}
       </ul>
-    </div>
+    </>
   )
 }
  
